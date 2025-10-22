bce2a3fc0f412cc65708f989dd7e862c94e8df9c
vixceity <phuongtranthimy071@gmail.com>
2025-10-22 10:00:43 -0400

    Create TopBlue.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/userSandboxes/PhuongCode/TopBlue.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode.userSandboxes.PhuongCode;
+package org.firstinspires.ftc.teamcode.pedroPathing; // make sure this aligns with class location
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.Path;
+import com.pedropathing.paths.PathChain;
+import com.pedropathing.util.Timer;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import  com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.bylazar.configurables.annotations.Configurable;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous(name = "TopBlue", group = "Linear OpMode")
+@Configurable
+public class TopBlue extends LinearOpMode {
+    private Follower follower;
+    private Timer pathTimer, actionTimer, opmodeTimer;
+    private int pathState;
+
+    
+}
