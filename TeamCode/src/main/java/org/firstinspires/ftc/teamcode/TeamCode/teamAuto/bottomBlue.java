1779b93ec449b747e81dc137d707816a8a49d370
alinaw <wasupanalina@gmail.com>
2025-10-23 17:12:22 -0400

    scrimm grind

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamCode/teamAuto/bottomBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamCode/teamAuto/bottomBlue.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.TeamCode;
+package org.firstinspires.ftc.teamcode.TeamCode.teamAuto;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamCode/temporaryServo.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.TeamCode;
+
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class temporaryServo {
+    public Servo servoArm;
+
+    public temporaryServo(HardwareMap hardwareMap){
+        servoArm = hardwareMap.get(Servo.class,"servoArm");
+    }
+
+    public void temporaryServoFunction (Gamepad gamepad2, Telemetry telemetry){
+        double servoPose;
+        if (gamepad2.yWasPressed()){
+            servoPose=1;
+            servoArm.setPosition(servoPose);
+        }else if (gamepad2.yWasReleased()){
+            servoPose=0;
+            servoArm.setPosition(servoPose);
+        }
+
+    }
+}
