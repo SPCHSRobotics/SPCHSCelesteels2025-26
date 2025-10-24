2347bc10ae8a7ec57a114f22f7d8dc68d6a18bb4
younicorn13xx <thunguye8002@gmail.com>
2025-10-24 12:27:06 -0400

    Update bottomRed.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamCode/teamAuto/bottomRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamCode/teamAuto/bottomRed.java
@@ -1,57 +1,16 @@
-/* Copyright (c) 2017 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
-package org.firstinspires.ftc.teamcode.TeamCode;
+package org.firstinspires.ftc.teamcode.TeamCode.teamAuto;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-/*
- * This OpMode illustrates the concept of driving a path based on time.
- * The code is structured as a LinearOpMode
- *
- * The code assumes that you do NOT have encoders on the wheels,
- *   otherwise you would use: RobotAutoDriveByEncoder;
- *
- *   The desired path in this example is:
- *   - Drive forward for 3 seconds
- *   - Spin right for 1.3 seconds
- *   - Drive Backward for 1 Second
- *
- *  The code is written in a simple form with no optimizations.
- *  However, there are several ways that this type of sequence could be streamlined,
- *
- * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
- */
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+
 
 @Autonomous(name="Scrimmage Bottom Red", group="Robot")
 public class bottomRed extends LinearOpMode {
@@ -61,11 +20,14 @@ public class bottomRed extends LinearOpMode {
    public DcMotor frontRightDrive = null;
    public DcMotor backLeftDrive = null;
    public DcMotor backRightDrive = null;
+    public DcMotor outtakeMotorLeft = null;
+    public DcMotor outtakeMotorRight = null;
 
-   private final ElapsedTime runtime = new ElapsedTime();
+    private final ElapsedTime runtime = new ElapsedTime();
 
 
     static final double     FORWARD_SPEED = 1;
+    static final double     SHOOTING_FULL_POWER = 1;
 
 
     @Override
@@ -76,6 +38,15 @@ public class bottomRed extends LinearOpMode {
         frontRightDrive = hardwareMap.get(DcMotor.class, "front_right_drive");
         backRightDrive = hardwareMap.get(DcMotor.class, "back_left_drive");
         backLeftDrive = hardwareMap.get(DcMotor.class, "back_right_drive");
+        //call outtake motor
+        outtakeMotorLeft = hardwareMap.get(DcMotor.class, "LeftOuttake");
+        outtakeMotorRight = hardwareMap.get(DcMotor.class, "RightOuttake");
+        //set outtake motor reverse
+        outtakeMotorLeft.setDirection(DcMotor.Direction.REVERSE);
+        outtakeMotorRight.setDirection(DcMotor.Direction.FORWARD);
+
+        outtakeMotorRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        outtakeMotorLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
         // When run, this OpMode should start both motors driving forward. So adjust these two lines based on your first test drive.
@@ -103,20 +74,18 @@ public class bottomRed extends LinearOpMode {
         while (opModeIsActive() && (runtime.seconds() < 3.0)) {
             telemetry.addData("Path", "Leg 1: %4.1f S Elapsed", runtime.seconds());
             telemetry.update();
-
-
-            // Step 3:  Drive Backward for 1 Second
-            frontLeftDrive.setPower(-FORWARD_SPEED);
+            //Step 2: Turn
+            frontLeftDrive.setPower(FORWARD_SPEED);
             frontRightDrive.setPower(-FORWARD_SPEED);
+            backLeftDrive.setPower(FORWARD_SPEED);
             backRightDrive.setPower(-FORWARD_SPEED);
-            backLeftDrive.setPower(-FORWARD_SPEED);
             runtime.reset();
             while (opModeIsActive() && (runtime.seconds() < 1.0)) {
-                telemetry.addData("Path", "Leg 3: %4.1f S Elapsed", runtime.seconds());
+                telemetry.addData("Path", "aiming", runtime.seconds());
                 telemetry.update();
             }
 
-            // Step 4:  Stop
+         // Step 3:  Stop
             frontLeftDrive.setPower(0);
             frontRightDrive.setPower(0);
             backLeftDrive.setPower(0);
@@ -125,6 +94,25 @@ public class bottomRed extends LinearOpMode {
             telemetry.addData("Path", "Complete");
             telemetry.update();
             sleep(1000);
-        }
+            //turn outtake on
+            outtakeMotorLeft.setPower(SHOOTING_FULL_POWER);
+            outtakeMotorRight.setPower(SHOOTING_FULL_POWER);
+            runtime.reset();
+            while (opModeIsActive() && (runtime.seconds() < 3.0)) {
+                telemetry.addData("Outtake Status:", "On", runtime.seconds());
+                telemetry.update();
+                //turn outtake off
+                outtakeMotorRight.setPower(0);
+                outtakeMotorLeft.setPower(0);
+                telemetry.addData("Outtake Status:", "Complete");
+                telemetry.update();
+                sleep(8000);
 
+
+            }
+        }
     }}
+
+
+
+
