ba65104f6ef28fec404b42fdf9f8839629c2ad31
ProgrammingMuse <167145418+ProgrammingMuse@users.noreply.github.com>
2025-10-30 12:55:37 -0400

    Auto pseudocode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/userSandboxes/lucyCode/lucyAutoCoordinatesTopBlue.java
@@ -0,0 +1,6 @@
+package org.firstinspires.ftc.teamcode.userSandboxes.lucyCode;
+
+public class lucyAutoCoordinatesTopBlue {
+    final int starting_position = 0;
+    final int leg_1 = starting_position + 12; // 12 inches forward
+}
