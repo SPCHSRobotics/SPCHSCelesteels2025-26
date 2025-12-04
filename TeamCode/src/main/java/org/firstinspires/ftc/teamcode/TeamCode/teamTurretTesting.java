57bbe2c117805018e0fd15c094bbe0eb57a9c0b9
younicorn13xx <thunguye8002@gmail.com>
2025-12-04 15:30:53 -0500

    Update teamTurretTesting.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamCode/teamTurretTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamCode/teamTurretTesting.java
@@ -1,35 +1,32 @@
 package org.firstinspires.ftc.teamcode.TeamCode;
 
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
-public class teamTurretTesting {
-    public DcMotor spinningMotor;
 
-    public teamTurretTesting(HardwareMap hardwareMap){
-        spinningMotor = hardwareMap.get(DcMotor.class, "spinning motor");
-        spinningMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-    }
-    public void teamTurretTestingFunction(Gamepad gamepad1, Telemetry telemetry){
-        double max;
+@TeleOp(name = "Thu tests Spinning Turret OpMode", group = "Linear OpMode")
+public class teamTurretTesting extends LinearOpMode {
 
-        double axial   = -gamepad1.left_stick_y;
-        double lateral =  -gamepad1.left_stick_x;
-        double yaw     =  gamepad1.right_stick_x;
+    @Override
+    public void runOpMode() {
+        // Wait for the game to start (driver presses START)
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
 
-        double spinningMotorPower = axial - lateral + yaw;
+        waitForStart();
 
-        max = (Math.abs(spinningMotorPower));
-        if (max > 1.0) {
-            spinningMotorPower /= max;
+        // run until driver presses STOP
+        while (opModeIsActive()) {
+            telemetry.update();
+        
         }
-        spinningMotor.setPower(spinningMotorPower);
-        telemetry.addData("Spinning motor", "%4.2f, %4.2f",spinningMotorPower, spinningMotorPower);
-    }
 
+    }
 
 }
